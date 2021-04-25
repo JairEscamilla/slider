e99952fe98d43afeedbfeeebe00d103fe8158a10
JairEscamilla <hernandez_yo14@outlook.es>
2021-04-25 18:20:02 -0500

    Configurando React con Typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  App: React.FC = () => {
+  return (
+    <div>
+      Hola mundo
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'awesome-typescript-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3005,
+  },
+}
